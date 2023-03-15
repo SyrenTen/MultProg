daed1b115295fe2032fced280eedd882e883c0c4
SyrenTen <vitaliay04@gmail.com>
2023-03-15 23:00:14 +0100

    single quotes everywhere

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@ print('Multi-paradigm programming language:Task 1')
 
 print('Maslova Vitaliia, i dont know')
 
-s = ("Vladislav Valeriyovich Ovsyannikov, " * 45)
+s = ('Vladislav Valeriyovich Ovsyannikov, ' * 45)
 print(s[:-2])
 
 print((11 + 2 * 3 + 4.1 / 12.4 - 221.3) + 4.8)
