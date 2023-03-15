2e307eeaa6884d80a0c169cdc3d3aa25c7eaba50
SyrenTen <vitaliay04@gmail.com>
2023-03-15 18:32:07 +0100

    fixed the code

--- task1.py
+++ task1.py
@@ -3,6 +3,6 @@ print('Multi-paradigm programming language:Task 1')
 print('Maslova Vitaliia, i dont know')
 
 s = ("Vladislav Valeriyovich Ovsyannikov, " * 45)
-
 print(s[:-2])
+
 print((11 + 2 * 3 + 4.1 / 12.4 - 221.3) + 4.8)
