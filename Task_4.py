5675d38ec06263bd6687f0907304d0bc33951e96
SyrenTen <vitaliay04@gmail.com>
2023-04-20 21:01:11 +0200

    complete task4

---
+++ Task_4.py
@@ -0,0 +1,14 @@
+#Multi-paradigm programming language: Task 4, Maslova Vitaliia, idk
+
+print('Multi-paradigm programming language: Task 4')
+
+print('Maslova Vitaliia, idk')
+
+n = int(input('Input n: '))
+sum = 0
+
+for i in range(1, n+1):
+    term = (2*i-1)/(2*i)
+    sum += term
+
+print('Sum = ', sum)
