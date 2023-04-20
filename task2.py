2915127bbd0536668046de888b56b14d050f58a9
SyrenTen <vitaliay04@gmail.com>
2023-04-20 20:22:44 +0200

    complete task2

---
+++ task2.py
@@ -0,0 +1,13 @@
+#Multi-paradigm programming language: Task 1, Maslova Vitaliia, idk
+
+print('Multi-paradigm programming language: Task 2')
+
+print('Maslova Vitaliia, idk')
+
+x = int(input('Input x: '))
+y = int(input('Input y: '))
+z = int(input('Input z: '))
+
+func = float((((11+2*x +4.1)/(12.4-y))+z))
+
+print(func)
