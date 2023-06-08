1e989e4f2d1211af0228a82331f46a9e3bcf11fa
SyrenTen <vitaliay04@gmail.com>
2023-06-08 14:09:11 +0200

    task_13: minesweeper in progress

--- task_13.py
+++ task_13.py
@@ -2,12 +2,13 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.popup import Popup
 
 from kivy.core.window import Window
 
 from random import randint
 
-Window.size = (500, 500)
+Window.size = (600, 600)
 Window.clearcolor = (0 / 255, 0 / 255, 0 / 255, 1)
 
 
@@ -15,17 +16,20 @@ Window.clearcolor = (0 / 255, 0 / 255, 0 / 255, 1)
 class Cell(Button):
     def __init__(self, **kwargs):
         super().__init__()
-        self.size = (90, 90)
         self.background_normal = 'cell.png'
-        self.flagged = False
-        self.mined = False
-        self.count_mined = 0
+        self.flagged = False  # поставлен флажок
+        self.mined = False  # является миной
+        self.clicked = False  # если нажата - состояние с цифрой
 
-    def place_flag(self):
+    def place_flag(self):  # размещение флага - надо привязать к ПКМ
         if self.flagged:
             self.background_normal = 'flag.png'
         else:
             self.background_normal = 'cell.png'
+
+    def cell_clicked(self, touch):
+        if touch.button == 'right':
+            self.background_normal = 'flag.png'
 
 
 class Minesweeper(GridLayout):
@@ -34,22 +38,32 @@ class Minesweeper(GridLayout):
         self.rows = 9
         self.cols = 9
         self.cells = []
-        self.create_buttons()
-        self.create_mines()
+        self.creating_buttons()
+        self.creating_mines()
 
-    def create_buttons(self):
+    def creating_buttons(self):
         for row in range(self.rows):
             for col in range(self.cols):
                 self.add_widget(Cell())
 
-    def create_mines(self):
+    def creating_mines(self):
         mine_pos = []
-        while len(mine_pos) < 10: # expected type list но все норм если len(mine_pos)
+        while len(mine_pos) < 10:  # expected type list но все норм если len(mine_pos)
             pos = (randint(0, self.rows), randint(0, self.cols))
             if pos not in mine_pos:  # нужно же 10 мин
                 mine_pos.append(pos)
-            cell = self.cells  # AttributeError надо поправить
-            cell.mined = True  # теперь эти клетки - мины
+                # cell = self.cells  # AttributeError надо поправить
+                # cell.mined = True  # теперь эти клетки - мины
+
+    def button_clicked(self, cell):
+        if cell.mined:
+            exit()  # добавить конец игры - ресет / отдельно?
+        else:
+            cell.clicked = True
+            # должна появится цифра
+
+
+
 
 
 
