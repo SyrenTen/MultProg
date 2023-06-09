801cb83b6c6795dc02b3bc4ab57d02deb85140a5
SyrenTen <vitaliay04@gmail.com>
2023-06-09 14:09:41 +0200

    done

--- task_13.py
+++ task_13.py
@@ -1,5 +1,4 @@
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
@@ -8,67 +7,139 @@ from kivy.core.window import Window
 
 from random import randint
 
-Window.size = (600, 600)
-Window.clearcolor = (0 / 255, 0 / 255, 0 / 255, 1)
+Window.size = (600, 600)  # для определения размера окна
 
 
-# у нас будет куча клеток(кнопок) и взаимодействия с ними поэтому создаем подскласс
-class Cell(Button):
+class Cell(Button):  # клас Cell наследуется от класса Button
     def __init__(self, **kwargs):
-        super().__init__()
+        super().__init__(**kwargs)
+        self.mine = False  # является миной
+        self.flagged = False  # для флажка
+        self.opened = False  # клетка открыта
         self.background_normal = 'cell.png'
-        self.flagged = False  # поставлен флажок
-        self.mined = False  # является миной
-        self.clicked = False  # если нажата - состояние с цифрой
 
-    def place_flag(self):  # размещение флага - надо привязать к ПКМ
-        if self.flagged:
-            self.background_normal = 'flag.png'
-        else:
-            self.background_normal = 'cell.png'
+    def on_touch_down(self, touch):  # on_touch_down(touch), запускается при нажатии
+        if self.collide_point(*touch.pos) and touch.button == 'right':  # collide_point для проверки в границах
+            self.flagged = not self.flagged  # переключает True - False
+            if self.flagged:
+                self.background_normal = 'flag.png'
+            else:
+                self.background_normal = 'cell.png'
+            return True
 
-    def cell_clicked(self, touch):
-        if touch.button == 'right':
-            self.background_normal = 'flag.png'
+        if self.collide_point(*touch.pos) and touch.button == 'left' and self.flagged:
+            return False  # возрращает False и предотвращает нажатие
+        return super(Cell, self).on_touch_down(touch)  # если ничего выше не произошло - просто нажатие
 
 
 class Minesweeper(GridLayout):
     def __init__(self, **kwargs):
-        super().__init__()
-        self.rows = 9
+        super().__init__(**kwargs)
         self.cols = 9
+        self.rows = 9
         self.cells = []
+        self.mine_count = 10
         self.creating_buttons()
         self.creating_mines()
+        self.counting_mines()
 
     def creating_buttons(self):
         for row in range(self.rows):
+            cell_row = []
             for col in range(self.cols):
-                self.add_widget(Cell())
+                cell = Cell()
+                cell.bind(on_release=self.button_clicked)  # on_release запускается при отпускании кнопки
+                self.add_widget(cell)
+                cell_row.append(cell)
+            self.cells.append(cell_row)
 
     def creating_mines(self):
         mine_pos = []
-        while len(mine_pos) < 10:  # expected type list но все норм если len(mine_pos)
-            pos = (randint(0, self.rows), randint(0, self.cols))
-            if pos not in mine_pos:  # нужно же 10 мин
+        while len(mine_pos) < 10:
+            pos = (randint(0, self.rows - 1), randint(0, self.cols - 1))
+            if pos not in mine_pos:
                 mine_pos.append(pos)
-                # cell = self.cells  # AttributeError надо поправить
-                # cell.mined = True  # теперь эти клетки - мины
+                cell = self.cells[pos[0]][pos[1]]
+                cell.mine = True
+
+    def counting_mines(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                cell = self.cells[row][col]
+                if not cell.mine:
+                    amount = 0
+                    for i in range(max(0, row - 1), min(row + 2, self.rows)):
+                        for j in range(max(0, col - 1), min(col + 2, self.cols)):
+                            if self.cells[i][j].mine:
+                                amount += 1
+                    cell.mine_count = amount
 
     def button_clicked(self, cell):
-        if cell.mined:
-            exit()  # добавить конец игры - ресет / отдельно?
+        if cell.mine:
+            self.game_end(False)  # если в клетке есть мина - конец игры
         else:
-            cell.clicked = True
-            # должна появится цифра
+            cell.opened = True
+            cell.text = str(cell.mine_count)
+            cell.disabled = True  # блокирует взаимодействие с клеткой
 
+            if cell.mine_count == 0:  # если в соседних клетках нет мин
+                self.opening_neighbors(cell)  # вызывается октрытие клеток
 
+            if self.check_win():
+                self.game_end(True)
 
+    def check_win(self):  # проверяет были ли открыты все клетки-не-мины
+        for row in self.cells:
+            for cell in row:
+                if not cell.opened and not cell.mine:
+                    return False
+        return True
 
+    def cell_pos(self, cell):  # для поиска позиции клетки
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.cells[row][col] == cell:
+                    return row, col
 
+    def opening_neighbors(self, cell):
+        row, col = self.cell_pos(cell)
+        for i in range(max(0, row - 1), min(row + 2, self.rows)):
+            for j in range(max(0, col - 1), min(col + 2, self.cols)):
+                adjacent_cell = self.cells[i][j]
 
+                if not adjacent_cell.opened and not adjacent_cell.mine:
+                    adjacent_cell.opened = True
+                    adjacent_cell.text = str(adjacent_cell.mine_count)
+                    adjacent_cell.disabled = True
+                    if adjacent_cell.mine_count == 0:
+                        self.opening_neighbors(adjacent_cell)
 
+    def game_end(self, stat):
+        if stat:  # то значение True
+            message = 'Win!'
+        else:
+            for row in self.cells:
+                for cell in row:
+                    if cell.mine:
+                        cell.background_normal = 'bomb.png'
+                    else:
+                        cell.opened = True
+                        cell.text = str(cell.mine_count)
+                        cell.disabled = True
+            message = 'Game over'
 
+        game_button = Button(text='New game', height=50)
+        game_button.bind(on_release=self.new_game)
+        popup = Popup(title=message, title_align='center', content=game_button,
+                      size_hint=(None, None), size=(150, 150))
+        popup.open()
+
+    def new_game(self, arg):  # начало новой игры, вызывается при нажатии кнопки
+        self.clear_widgets()  # очищает виджеты
+        self.cells = []
+        self.creating_buttons()
+        self.creating_mines()
+        self.counting_mines()
 
 
 class GameMinesweeper(App):
