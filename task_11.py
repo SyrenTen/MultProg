d4431fdcf5ee5e0ce17b2cfcbb3696411dec8609
SyrenTen <vitaliay04@gmail.com>
2023-06-11 02:05:27 +0200

    fixed mistakes

--- task_11.py
+++ task_11.py
@@ -1,34 +1,22 @@
 import re
 
 
-def find_orientation(name):
+def find_orientation(article_name):
     positive_words = ['good', 'great', 'excellent', 'positive', 'incredible', 'amazing']
     negative_words = ['bad', 'terrible', 'poor', 'negative', 'awful']
 
-    with open(name, 'r') as task11:
+    with open(article_name, 'r') as task11:
         article = task11.read().lower()
 
-    words = re.findall(r'\b\w+\b', article)
+    count_pos = sum(1 for word in article.split() if any(re.search(r'\b{}\b'.format(pos_word),
+                                                                   word) for pos_word in positive_words))
+    count_neg = sum(1 for word in article.split() if any(re.search(r'\b{}\b'.format(neg_word),
+                                                                   word) for neg_word in negative_words))
 
-    count_pos = 0
-    count_neg = 0
-
-    for word in words:
-        if any(re.search(r'\b{}\b'.format(pos_word), word) for pos_word in positive_words):
-            count_pos += 1
-        elif any(re.search(r'\b{}\b'.format(neg_word), word) for neg_word in negative_words):
-            count_neg += 1
-
-    if count_pos > count_neg:
-        orientation = 'Positive'
-    elif count_pos == count_neg:
-        orientation = 'Neutral'
-    else:
-        orientation = 'Negative'
+    orientation = 'Positive' if count_pos > count_neg else 'Neutral' if count_pos == count_neg else 'Negative'
 
     print(f'Article orientation: {orientation}')
 
 
 if __name__ == '__main__':
-    article_name = 'article_for_task_11.txt'
-    find_orientation(article_name)
+    find_orientation('article_for_task_11.txt')
